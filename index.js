a848a8886300baa2f5c9a665937812fe271bce24
MarliRenee <61030628+MarliRenee@users.noreply.github.com>
2020-03-31 15:47:00 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -12,18 +12,40 @@
     //     <span class="shopping-item">apples</span>
     //     <div class="shopping-item-controls">
 
+    // <li>
+    //     <span class="shopping-item">apples</span>
+    //     <div class="shopping-item-controls">
+    //       <button class="shopping-item-toggle">
+    //         <span class="button-label">git check</span>
+    //       </button>
+    //       <button class="shopping-item-delete">
+    //         <span class="button-label">delete</span>
+    //       </button>
+    //     </div>
+    //   </li>
+
 
     $('#js-shopping-list-form').on('submit', function addItem(e){
         e.preventDefault();
         var text = $('#shopping-list-entry').val();
-        $('.shopping-list').append('<li>' + text + '</li>');
+        $('.shopping-list').append('<li>' + '<span class="shopping-item">' + text + '</span>' + 
+            + '<div class="shopping-item-controls">' 
+            + '<button class="shopping-item-toggle">'
+                + '<span class="button-label">check</span>'
+            + '</button>'
+            + '<button class="shopping-item-delete">'
+                + '<span class="button-label">delete</span>'
+            +'</button>'
+            + '</div>' 
+        + '</li>');
       });
+            
+{/* //       </div> + text + '</li>' */}
 
-
-$('.shopping-list').addItem(function(){
-    e.preventDefault();
-    $('.li').addClass('shopping-item');
-});
+// $('.shopping-list').addItem(function(){
+//     e.preventDefault();
+//     $('li').toggleClass('shopping-item');
+// });
 
 
 // $(document).ready(function(){
